54653b4429d872e44033262adc3588382abe3b51
felipera8732 <felipe@pruvo.com>
2023-04-17 16:45:26 -0300

    Added stitches and theme configuration using token package

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,42 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    // if this is not here, all the properties will be overwritten completely. This way
+    // both the default values and mapped ones will be available on intellisense
+    ...defaultThemeMap,
+
+    // spacing on theme uses only for padding and margin, this is a way to map
+    // the spaces to another css property, intellisense will now display the values
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
